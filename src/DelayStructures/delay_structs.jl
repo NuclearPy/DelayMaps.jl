b0645ecbcd954cc98441046335010fce52f26f90
Adam Zaidan <adam.zaidan@gatech.edu>
2025-05-18 13:20:18 -0400

    Changed comment

--- src/DelayStructures/delay_structs.jl
+++ src/DelayStructures/delay_structs.jl
@@ -5,7 +5,7 @@ Abstract type for all delay maps.
 """
 abstract type DelayMap{T<: Function, S <: Function} end
 
-# Delay maps are functions from C[-1,1] -> C[-1,1], these constants hardcode this fact into the code
+# Delay maps are functions from C[-1,1] -> C[-1,1]
 const PHASE_SPACE_INTERVAL_START = -1.0
 const PHASE_SPACE_INTERVAL_END = 1.0
 const PHASE_SPACE_INTERVAL_LENGTH = PHASE_SPACE_INTERVAL_END - PHASE_SPACE_INTERVAL_START
