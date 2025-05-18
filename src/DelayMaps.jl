f809e11832b82d7687bd7e7ae95e52c6bb718fe9
Adam Zaidan <adam.zaidan@gatech.edu>
2025-05-18 13:19:49 -0400

    Added ExplicitDelayMap.jl

--- src/DelayMaps.jl
+++ src/DelayMaps.jl
@@ -5,6 +5,9 @@ using RadiiPolynomial
 include("DelayStructures/delay_structs.jl")
     export DelayMap, ExplicitDelayMap, Jacobian
 
+include("DelayStructures/ExplicitDelayMap.jl")
+    export ExplicitDelayMap
+
 include("rotation.jl")
     export f
 
